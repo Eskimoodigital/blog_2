6bb607dde9efd440fab3320415c99530ec97aa95
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-08-04 09:46:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,36 @@
-#blog2
+
+
+# Terraform for Infrastructure Architects - Part 2
+
+#define the terraform providers
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+#define the AWS region
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+
+#define AWS resources
+
+
+resource "aws_instance" "mcna_ec2" {
+    ami = "ami-00f22f6155d6d92c5"
+    instance_type = "t2.micro"
+
+}
+
+output "publicdns" {
+    value = aws_instance.eskimoo16661.public_dns
+}
+
